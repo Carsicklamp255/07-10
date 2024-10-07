23e5f24f4d9237a5fcf94bb572afcb34be7aa782
Carsicklamp255 <160789738+Carsicklamp255@users.noreply.github.com>
2024-10-07 08:41:35 -0300

    Add files via upload

---
+++ piramide/desafio60.py
@@ -0,0 +1,7 @@
+def solve(equation):
+    result = 0
+    return result
+
+print(solve('2 + 3'))
+print(solve('2 - 1 + 2'))
+print(solve('(1 + (4+5+2)-3)'))
---
+++ piramide/teste.py
@@ -0,0 +1,3 @@
+numeros = [1, 2, 3, 4]
+resultado = sum(numeros)
+print("Resultado da soma:", resultado)
